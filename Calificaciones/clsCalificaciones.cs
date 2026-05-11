62183d1ac10ada4ad20abe03ee791ccb0faf08b9
@LMendoza70 <luis.mendoza@uthh.edu.mx>
2026-05-11 08:41:22 -0600

    Renombrar clsCalificaciones a Materia y refactorizar clase
    Se renombra la clase a Materia, se actualizan los nombres de los atributos a camelCase, se agregan propiedades públicas con validación para los parciales, se modifican los constructores y se renombra el método promedio a CalcularPromedio.

--- Calificaciones/clsCalificaciones.cs
+++ Calificaciones/clsCalificaciones.cs
@@ -7,38 +7,93 @@ using System.Threading.Tasks;
 namespace Calificaciones.Calificaciones
 {
     //se ha creado la clase calificaciones 
-    internal class clsCalificaciones
+    internal class Materia
     {
         //segun el proceso de abstraccion se detecta que cada materia contiene los siguientes atributos y metodos 
         //atributos
-        private string Nombre;
-        private float Parcial_1;
-        private float Parcial_2;
-        private float Parcial_3;
-        
-        //constructor vacio
-        public clsCalificaciones()
+        private string nombre;
+        private float parcial1;
+        private float parcial2;
+        private float parcial3;
+
+        //propiedades de los atributos
+
+        public float Parcial1
         {
-            Parcial_1 = 0.0f;
-            Parcial_2 = 0.0f;
-            Parcial_3 = 0.0f;
-            Nombre = "";
+            get { return parcial1; }
+            set
+            {
+                if(value>=0 && value <= 10)
+                {
+                    parcial1 = value;
+                }
+                else
+                {
+                    parcial1 = -1f;
+                }
+            }
+        }
+
+        public float Parcial2
+        {
+            get { return parcial2; }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                {
+                    parcial2 = value;
+                }
+                else
+                {
+                    parcial2 = -1f;
+                }
+            }
+        }
+
+        public float Parcial3
+        {
+            get { return parcial3; }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                {
+                    parcial3 = value;
+                }
+                else
+                {
+                    parcial3 = -1f;
+                }
+            }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        //constructor vacio
+        public Materia()
+        {
+            parcial1 = 0.0f;
+            parcial2 = 0.0f;
+            parcial3 = 0.0f;
+            nombre = "";
         }
 
         //constructor parametrizado 
-        public clsCalificaciones(string nombre, float P1, float p2, float p3)
+        public Materia(string nombre, float P1, float p2, float p3)
         {
-            Parcial_1 = P1;
-            Parcial_2 = p2;
-            Parcial_3 = p3;
-            Nombre = nombre;
+            parcial1 = P1;
+            parcial2 = p2;
+            parcial3 = p3;
+            this.nombre = nombre;
         }
 
         //metodos
-        public float promedio()
+        public float CalcularPromedio()
         {
             float res = 0;
-            res = (Parcial_1 + Parcial_2 + Parcial_3) / 3;
+            res = (parcial1 + parcial2 + parcial3) / 3;
             return res;
         }
 

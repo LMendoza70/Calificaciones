11f5e97e51c685f9051c10f3e604956158d651e3
MTI.Luis-Mendoza <luis.mendoza@uthh.edi.mx>
2026-05-18 09:27:53 -0600

    Añade clase Bebida con atributos y métodos básicos
    Se crea el archivo Bebida.cs en Calificaciones.Cafeteria, definiendo la clase interna Bebida con atributos privados, propiedades, constructores y métodos para preparar bebidas y aplicar descuentos.

---
+++ Cafeteria/Bebida.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calificaciones.Cafeteria
+{
+    internal class Bebida
+    {
+        //atributos 
+        private string Nombre;
+        private string Tamaño;
+        private float Precio;
+
+        //propiedades
+        public string nombre
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+
+        public string tamaño {  
+            get { return Tamaño; }
+            set { Tamaño = value; }
+        }
+
+        private float precio
+        {
+            get { return Precio; }
+            set {  if (value > 0) 
+                    Precio = value;
+                }
+        }
+
+        //constructor parametrizado
+        public Bebida(string _nombre, string _tamaño, float _precio)
+        {
+            Nombre = _nombre;
+            Tamaño = _tamaño;
+            Precio= _precio;
+        }
+
+        // contructor vacio
+        public Bebida()
+        {
+            Nombre = "";
+            Tamaño = "";
+            Precio = 0.0F;
+        }
+
+        //metodos 
+        public string Preparar()
+        {
+            return "Preparando un : "+ Nombre + " de tamaño : "+Tamaño;
+        }
+
+        public void Descuento(float descuento)
+        {
+            Precio = Precio * (1 - (descuento / 100));
+        }
+
+    }
+}
